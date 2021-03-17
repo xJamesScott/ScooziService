1110cc4718eb11b613233fc04521492b3f172f03
James <50638563+xJamesScott@users.noreply.github.com>
2021-03-16 21:21:32 -0400

    edits to about

--- faq-card.js
+++ faq-card.js
@@ -1,14 +1,50 @@
 var coll = document.getElementsByClassName("faq-button");
-var i;
+var collMark = document.getElementsByClassName("faq-button");
+// var i;
 
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
-    });
+// for (var i = 0; i < coll.length; i++) {
+//     coll[i].addEventListener("click", function () {
+//         this.classList.toggle("active");
+//         var content = this.nextElementSibling;
+//         if (content.style.maxHeight) {
+//             content.style.maxHeight = null;
+//             content.style.height = "50rem";
+//         } else {
+//             content.style.maxHeight = content.scrollHeight + "2rem";
+//             content.style.height = "0";
+//         }
+//     });
+// }
+
+//Try 2
+
+// var mark2 = document.querySelector("mark");
+// for (var i = 0; i < coll.length; i++) {
+//     coll[i].addEventListener("click", function () {
+//         this.classList.toggle("active");
+//         var content = this.nextElementSibling;
+//         content.classList.toggle("accordion-open");
+
+//         mark2.classList.toggle("accordion-open")
+
+//     });
+// }
+
+//Try 3
+
+var accItem = document.getElementsByClassName('faq-button');
+var accHD = document.getElementsByClassName('panels');
+for (i = 0; i < accHD.length; i++) {
+    accHD[i].addEventListener('click', toggleItem, false);
 }
+function toggleItem() {
+    var itemClass = this.parentNode.className;
+    for (i = 0; i < accItem.length; i++) {
+        accItem[i].className = 'closed';
+    }
+    if (itemClass == 'closed') {
+        this.parentNode.className = 'accordion-open';
+    }
+}
+
+
