9d4a2e26b8f7868125a39a87dbd125efd496a1dd
xJamesScott <50638563+xJamesScott@users.noreply.github.com>
2021-03-22 23:39:16 -0400

    mobile menu script

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ const test = "This is script"
 
 document.getElementById("yo").innerHTML = test;
 
+const menuOpen = document.getElementById('mobile-menu-open')
+const menuClose = document.getElementById('mobile-menu-closed')
+const hideOpen = menuOpen.classList.remove("show")
+const setMenuClose = menuClose.classList.add("show")
+const setMenuOpen = menuOpen.classList.add("show")
+
+const menuToggle = () => {
+    if (menuOpen.classList = "show") {
+        hideOpen()
+        showClose()
+    };
+    else {
+    setMenuClose()
+    setMenuOpen()
+}
+};
+
