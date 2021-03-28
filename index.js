4549a7961eb861ded8c5f03f713295f0a4c0c2b4
James <50638563+xJamesScott@users.noreply.github.com>
2021-03-28 15:15:38 -0400

    responsive edits

--- index.js
+++ index.js
@@ -5,21 +5,36 @@
 
 const menuOpen = document.getElementById('mobile-menu-open');
 const menuClose = document.getElementById('mobile-menu-closed');
-const hideOpen = menuOpen.classList.remove("show");
-const setMenuClose = menuClose.classList.add("show");
-const setMenuOpen = menuOpen.classList.add("show");
+const navMenu = document.getElementById('mobile-nav-menu');
+const dimmer = document.getElementById('dimmer');
+// const hideOpen = () => { menuOpen.classList.remove("show") };
+// const hideClose = () => { menuClose.classList.remove("show") };
+// const showMenuClose = () => { menuClose.classList.add("show, bro") };
+// const showMenuOpen = () => { menuOpen.classList.add("show") };
+
+const toggleOpen = () => { menuOpen.classList.toggle("show") };
+const toggleClose = () => { menuClose.classList.toggle("show") };
+const toggleMenu = () => { navMenu.classList.toggle("show") }
+const toggleDim = () => { dimmer.classList.toggle("show") }
 
 // const pink = function (e) { alert("yoo!") }
 
 const menuToggle = () => {
-    alert("menuToggle!")
-    if (menuOpen.classList = "show") {
-        hideOpen()
-        showClose()
-    } else {
-        setMenuClose()
-        setMenuOpen()
-    }
-};
+    // alert("menuToggle!")
+    // setMenuOpen();
+    // if (menuOpen.classList = "show") {
+    //     hideOpen()
+    //     showMenuClose()
+    // } else {
+    //     hideClose()
+    //     showMenuOpen()
+    // }
 
+    toggleOpen()
+    toggleClose()
+    toggleMenu()
+    toggleDim()
+
+};
+menuClose.addEventListener("click", (e) => { menuToggle() });
 menuOpen.addEventListener("click", (e) => { menuToggle() });
