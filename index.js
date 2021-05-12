d9c236e81b906c9e0def18ab13fa0876ec01c61f
xJamesScott <50638563+xJamesScott@users.noreply.github.com>
2021-05-11 21:51:37 -0400

    scripts cleanup

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+// Mobile menu
+
 const menuOpen = document.getElementById('mobile-menu-open');
 const menuClose = document.getElementById('mobile-menu-closed');
 const navMenu = document.getElementById('mobile-nav-menu');
@@ -14,23 +16,20 @@ const menuToggle = () => {
     toggleClose()
     toggleMenu()
     toggleDim()
-
 };
 menuClose.addEventListener("click", (e) => { menuToggle() });
 menuOpen.addEventListener("click", (e) => { menuToggle() });
 
+// Request form modal
+
 const inviteForm = document.getElementById('invite-form')
 const dimmerForm = document.getElementById('dimmer-form');
-
 const toggleDimForm = () => { dimmer.classList.toggle("show") }
-
 const inviteFormToggle = () => {
     inviteForm.classList.toggle("form-visible")
-
 }
 
 const formTriggerButtons = document.getElementsByClassName('form-button')
-
 for (let i = 0; i < formTriggerButtons.length; i++) {
     formTriggerButtons[i].addEventListener("click",
         (e) => {
@@ -41,42 +40,21 @@ for (let i = 0; i < formTriggerButtons.length; i++) {
     )
 }
 
+// Map marker hover
+
 const locationBox = document.getElementsByClassName('location-box')
-
 const locationBg = document.getElementsByClassName('location-bg')
-// const locationBgOpacity = (e) => { e.style.opacity = "50%" }
-// const locationBgOpacityReset = (e) => { e.style.opacity = "1" }
-
 const locationBgOpacity = (e) => e.classList.toggle('location-hover')
 
 for (let i = 0; i < locationBox.length; i++) {
     console.log({locationBox: locationBox})
     locationBox[i].addEventListener('mouseover',
         () => {
-            console.log("yo!")
-            // console.log(locationBox[i].style.opacity)
-            // console.log({ bg: locationBg[i] })
             locationBgOpacity(locationBg[i])
-            // locationBox[i].addEventListener( 'mouseout',
-            // () => { 
-            //     locationBgOpacityReset(locationBg[i])
-            // }, false
-            // )
-
         })
     locationBox[i].addEventListener('mouseout',
         () => {
-
-            console.log("mouseout!")
-            // console.log(locationBox[i].style.opacity)
-            // console.log({ bg: locationBg[i] })
             locationBgOpacity(locationBg[i])
-            // locationBox[i].addEventListener( 'mouseout',
-            // () => { 
-            //     locationBgOpacityReset(locationBg[i])
-            // }, false
-            // )
-
         })
 }
 
